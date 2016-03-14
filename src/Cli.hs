f5073df11bf660cc664daa2d5547fd56e9da7a22
Tim Bussmann <tim.bussmann@particular.net>
2016-03-14 21:53:29 +0100

    reworked gameloop

--- src/Cli.hs
+++ src/Cli.hs
@@ -33,31 +33,63 @@ start = do
 
   let handleInput = handle Nothing
 
-  Cli.repeat (handleInput >=> (\(c, _) -> return c)) getLine
+  --Cli.repeat (handleInput >=> (\(c, _) -> return c)) (\b -> getLine >>= (\i -> handle b i)
+  loop (\b -> getLine >>= handle b) Nothing
 
   putStrLn "Goodbye!"
   return ()
 
-
-repeat :: (a -> IO Bool) -> IO a -> IO ()
-repeat check action = do
-  x <- action
-  continue <- check x
-  when continue $ Cli.repeat check action
-
+loop :: (Maybe Board -> IO (Bool, Maybe Board)) -> Maybe Board -> IO ()
+loop action board = do
+  result <- action board
+  when (fst result) $ loop action (snd result)
 
 handle :: Maybe Board -> String -> IO (Bool, Maybe Board)
-handle board "board" = do
-  print board
-  return (True, board)
 handle b "help" = do
   putStrLn "exit = leave the game"
   putStrLn "board = shows the current board"
   return (True, b)
 handle b "exit" =
   return (False, b)
+handle Nothing "start" = do
+  let board = endTurn createNewBoard
+  putStrLn "Started new game!"
+  return (True, Just board)
+handle Nothing _ = do
+  putStrLn "No board found. Please start a new game."
+  return (True, Nothing)
+handle (Just board) "board" = do
+  print board
+  return (True, Just board)
 handle (Just b) "next" =
   return (True, Just (endTurn b))
 handle b c = do
   putStrLn $ printf "unknown command \"%s\". Type \"help\" to list available commands." c
   return (True, b)
+
+createNewBoard :: Board
+createNewBoard =
+  let
+  {
+    player1 = Player { name = "player1"
+                      , hand = []
+                      , public = []
+                      , deck = [ Card 1 1 1
+                               , Card 2 2 2
+                               , Card 3 3 3
+                      ]
+                    , totalMana = 0
+                    , currentMana= 0
+                    , hp = 0};
+    player2 = Player { name = "player2"
+                      , hand = []
+                      , public = []
+                      , deck = [ Card 1 1 1
+                               , Card 2 2 2
+                               , Card 3 3 3
+                      ]
+                    , totalMana = 0
+                    , currentMana= 0
+                    , hp = 0}
+  }
+  in Board player1 player2
