679627e8eeca57ab0b47e45f0e33e3fafcc20313
Tim Bussmann <tim.bussmann@particular.net>
2016-03-29 22:33:42 +0200

    added args parameter access

--- src/Cli.hs
+++ src/Cli.hs
@@ -4,6 +4,7 @@ import Game
 import System.IO
 import Control.Monad
 import Text.Printf
+import Data.List.Split
 
 start = do
   putStrLn "Welcome to hStone"
@@ -34,7 +35,7 @@ start = do
   let handleInput = handle Nothing
 
   --Cli.repeat (handleInput >=> (\(c, _) -> return c)) (\b -> getLine >>= (\i -> handle b i)
-  loop (\b -> getLine >>= handle b) Nothing
+  loop (\b -> getLine >>= (\i -> handle b (splitOn " " i))) Nothing
 
   putStrLn "Goodbye!"
   return ()
@@ -44,26 +45,31 @@ loop action board = do
   result <- action board
   when (fst result) $ loop action (snd result)
 
-handle :: Maybe Board -> String -> IO (Bool, Maybe Board)
-handle b "help" = do
+handle :: Maybe Board -> [String] -> IO (Bool, Maybe Board)
+handle b ("help":_) = do
   putStrLn "exit = leave the game"
   putStrLn "board = shows the current board"
+  putStrLn "start = start a new game"
+  putStrLn "end = end the current turn"
+  putStrLn "attack {attackerId} {targetId} = attacks the target with the given attacker"
   return (True, b)
-handle b "exit" =
+handle b ("exit":_) =
   return (False, b)
-handle Nothing "start" = do
+handle Nothing ("start":_) = do
   let board = endTurn createNewBoard
   putStrLn "Started new game!"
   return (True, Just board)
 handle Nothing _ = do
   putStrLn "No board found. Please start a new game."
   return (True, Nothing)
-handle (Just board) "board" = do
+handle (Just board) ("board":_) = do
   print board
   return (True, Just board)
-handle (Just b) "next" =
+handle (Just b) ("end":_) =
   return (True, Just (endTurn b))
-handle b c = do
+handle (Just b) ("attack":_)=
+  return (True, Just b)
+handle b (c:_) = do
   putStrLn $ printf "unknown command \"%s\". Type \"help\" to list available commands." c
   return (True, b)
 
