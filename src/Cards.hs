f87e66ac198f64950d4e65d1c9c3ea6cb03d9938
Tim Bussmann <tim.bussmann@particular.net>
2016-06-21 21:03:23 +0200

    sample cards

---
+++ src/Cards.hs
@@ -0,0 +1,17 @@
+module Cards where
+
+import Game;
+
+cards = [
+  Card "Faceless Behemoth" 10 10 10,
+  Card "Captured Jormungar" 5 9 7,
+  Card "Core Hound" 9 5 7,
+  Card "Boulderfist Ogre" 6 7 7,
+  Card "Chillwind Yeti" 4 5 4,
+  Card "Oasis Snapjaw" 2 7 4,
+  Card "Magma Rager" 5 1 3,
+  Card "Blodfen Raptor" 3 2 2,
+  Card "River Crocolisk" 2 3 2,
+  Card "Murloc Raider" 2 1 1,
+  Card "Wisp" 1 1 0
+  ]
